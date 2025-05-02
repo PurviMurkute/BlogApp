fe8923dee8a3cc3bc8b0632bde63381790bf94b0
PurviMurkute <purvimurkute51@gmail.com>
2025-05-03 00:43:35 +0530

    blog app ready

--- src/components/BlogData.jsx
+++ src/components/BlogData.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router'
 export const BlogData = ({ id, title, author, date, category, excerpt }) => {
   return (
     <Link to={`/read/${id}/${category}`}>
-    <div className='border-2 border-gray-300 bg-gray-50 mx-5 my-7 py-3 px-5 rounded-xl shadow-lg hover:shadow-xl hover:bg-gray-100 hover:border-gray-300'>
-      <h1 className='text-xl font-bold pb-1'>{title}</h1>
+    <div className='border-2 border-gray-300 bg-gray-50 mx-10 my-7 py-4 px-7 rounded-xl shadow-lg hover:shadow-xl hover:bg-gray-100 hover:border-gray-300'>
+      <h1 className='text-xl font-bold pb-1'>{id}. {title}</h1>
       <div className='flex'>
       <p className='me-3'>Author: <span className='font-bold'>{author}</span></p>
       <p className=''>Published On: <span className='font-bold'>{date}</span></p>
--- src/views/Read.jsx
+++ src/views/Read.jsx
@@ -8,7 +8,7 @@ export const Read = () => {
   const [blogsData, setBlogData] = useState({
     id: null,
     title: "",
-    author: "Aarav Patel",
+    author: "",
     date: "",
     category: "",
     excerpt: "",
@@ -24,8 +24,13 @@ export const Read = () => {
   }, [id, category]);
   return (
     <div>
-      <h1 className="text-2xl font-bold m-5 text-center">{blogsData.title}</h1>
-      <img src={blogsData.thumbnail} alt="img" className="h-[400px] block mx-auto"/>
+      <h1 className="text-4xl font-bold m-5 py-3 text-center bg-gray-100">{blogsData.title}</h1>
+      <div className="flex justify-evenly my-5">
+      <p>Published On : <strong>{blogsData.date}</strong></p>
+      <p >By : <strong>{blogsData.author}</strong></p>
+      </div>
+      <img src={blogsData.thumbnail} alt="img" className="h-[450px] block mx-auto rounded-lg bg-gray-100 px-10 py-5"/>
+      <p className="bg-gray-100 mx-50 my-7 text-justify px-20 py-10 text-md">{blogsData.excerpt}</p>
     </div>
   );
 };
