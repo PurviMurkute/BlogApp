7b65086cc97cd8973b3f22963926235ca6481f8c
PurviMurkute <purvimurkute51@gmail.com>
2025-05-03 20:16:13 +0530

    responsive app

--- src/components/BlogData.jsx
+++ src/components/BlogData.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router'
 export const BlogData = ({ id, title, author, date, category, excerpt }) => {
   return (
     <Link to={`/read/${id}/${category}`}>
-    <div className='border-2 border-gray-300 bg-gray-50 mx-10 my-7 py-4 px-7 rounded-xl shadow-lg hover:shadow-xl hover:bg-gray-100 hover:border-gray-300'>
-      <h1 className='text-xl font-bold pb-1'>{id}. {title}</h1>
-      <div className='flex'>
+    <div className='border-2 border-gray-300 bg-gray-50 mx-5 md:mx-10 my-7 py-4 px-5 md:px-7 rounded-xl shadow-lg hover:shadow-xl hover:bg-gray-100 hover:border-gray-300'>
+      <h1 className='text-lg md:text-xl font-bold pb-1 text-justify'>{id}. {title}</h1>
+      <div className='flex my-1'>
       <p className='me-3'>Author: <span className='font-bold'>{author}</span></p>
       <p className=''>Published On: <span className='font-bold'>{date}</span></p>
       </div>
-      <p>{excerpt.length > 100 ? excerpt.substring(0, 150) + "..." : excerpt}</p>
+      <p className='text-justify'>{excerpt.length > 100 ? excerpt.substring(0, 150) + "..." : excerpt}</p>
     </div>
     </Link>
   )
--- src/views/Read.jsx
+++ src/views/Read.jsx
@@ -24,13 +24,13 @@ export const Read = () => {
   }, [id, category]);
   return (
     <div>
-      <h1 className="text-4xl font-bold m-5 py-3 text-center bg-gray-100">{blogsData.title}</h1>
-      <div className="flex justify-evenly my-5">
-      <p>Published On : <strong>{blogsData.date}</strong></p>
+      <h1 className="text-xl md:text-4xl font-bold m-5 py-3 text-center bg-gray-100">{blogsData.title}</h1>
+      <div className="flex justify-evenly my-5 mx-5 md:mx-1">
+      <p className="me-3 md:me-1">Published On : <strong>{blogsData.date}</strong></p>
       <p >By : <strong>{blogsData.author}</strong></p>
       </div>
-      <img src={blogsData.thumbnail} alt="img" className="h-[450px] block mx-auto rounded-lg bg-gray-100 px-10 py-5"/>
-      <p className="bg-gray-100 mx-50 my-7 text-justify px-20 py-10 text-md">{blogsData.excerpt}</p>
+      <img src={blogsData.thumbnail} alt="img" className="h-[200px] md:h-[450px] block mx-auto rounded-lg bg-gray-100 px-2 md:px-10 py-5"/>
+      <p className="bg-gray-100 mx-4 md:mx-50 my-5 md:my-7 text-justify px-5 md:px-20 py-4 md:py-10 text-md md:text-lg">{blogsData.excerpt}</p>
     </div>
   );
 };
